33959ae60277255a2c472e3e87d904669e412298
oli <oli.desborough@ovoenergy.com>
2019-05-10 16:26:15 +0100

    Fixed JVM opts

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [uncomplicate/neanderthal "0.23.1"]]
+  :jvm-opts ["--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]
   :repl-options {:init-ns scratch2gpu.core})
